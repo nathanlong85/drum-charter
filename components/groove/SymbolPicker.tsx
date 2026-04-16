52a45174ca80a6d646c94a9fb182bf9c8f8b37d0
Nathan Long <nathanlong85@gmail.com>
2026-04-16 16:16:45 -0400

    fix(e2e): improve test stability and resolve linting conflicts

--- components/groove/SymbolPicker.tsx
+++ components/groove/SymbolPicker.tsx
@@ -91,7 +91,7 @@ export const SymbolPicker: React.FC<SymbolPickerProps> = ({
 
       setAdjustedPos({ top, left });
     }
-  }, [position, category]);
+  }, [position]);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
